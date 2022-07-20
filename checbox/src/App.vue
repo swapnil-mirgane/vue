c41d8a9a09c69620a449e37d7a73c2adad1cd07c
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-07-20 14:10:58 +0530

    loop components are aded

--- checbox/src/App.vue
+++ checbox/src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <gender/>
+  <loop/>
+  <sele/>
 
 </template>
 
 <script>
 import gender from './components/ChecBox.vue';
-
+import loop from './components/loop.vue';
+import sele from './components/Check.vue';
 export default {
   name: 'App',
   components: {
     gender,
+    loop,
+    sele,
     
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ checbox/src/components/loop.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<ul>
+    <li v-for="item in arr" :key="item" >{{item}}</li>
+</ul>
+</div>
+</template>
+<script>
+export default {
+    name:'looP',
+    data() {
+        return {
+            arr:['swapnil','kunal','rohit','ganesh'],
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
